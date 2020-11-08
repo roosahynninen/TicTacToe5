ad1434a214572a9e88f4db6c2b85daa7c9b08391
roosahynninen <63446491+roosahynninen@users.noreply.github.com>
2020-11-08 22:27:28 +0200

    nonyt

--- src/play.js
+++ src/play.js
@@ -1,165 +1,161 @@
-var counter = 0;
-
-var board = document.getElementById("board");
+var count = 0;
 var table = document.createElement("table");
-var row;
-var cell;
-var text;
 
-createTable();
+//the reference for the body
+var board = document.getElementById("board");
 
 function createTable() {
-  for (var i = 0; i < 5; i++) {
-    row = table.insertRow();
-
-    for (var j = 0; j < 5; j++) {
-      cell = row.insertCell();
-      text = document.createTextNode("");
-      cell.appendChild(text);
+  // create rows
+  for (var r = 0; r < 5; r++) {
+    var t_row = table.insertRow();
+    // create cells in row
+    for (var c = 0; c < 5; c++) {
+      var t_cell = t_row.insertCell();
+      var cellText = document.createTextNode("");
+      t_cell.appendChild(cellText);
     }
   }
   board.appendChild(table);
-  onClick();
+  onClick(table);
 }
 
-function onClick() {
-  for (var i = 0; i < table.rows.length; i++) {
-    for (var j = 0; j < table.rows[i].cells.length; j++) {
-      table.rows[i].cells[j].onclick = function () {
-        fill(this);
+function onClick(table) {
+  for (var t = 0; t < table.rows.length; t++) {
+    for (var i = 0; i < table.rows[t].cells.length; i++) {
+      table.rows[t].cells[i].onclick = function () {
+        fillCell(table, this);
       };
     }
   }
 }
 
-function fill(cell) {
-  if (counter % 2 === 0) {
-    if (cell.innerHTML === "") {
-      cell.innerHTML = "X";
-      winCheck();
-      drawCheck();
-      counter++;
-      document.getElementById("turnIndicator").innerHTML = "Player 2's turn";
+function fillCell(table, t_cell) {
+  count++;
+  if (count % 2 === 0) {
+    if (t_cell.innerHTML === "") {
+      t_cell.innerHTML = "O";
     } else {
-      alert("Pick a free cell");
+      alert("That cell is already chosen");
+      count--;
     }
   } else {
-    if (cell.innerHTML === "") {
-      cell.innerHTML = "O";
-      winCheck();
-      drawCheck();
-      counter++;
-      document.getElementById("turnIndicator").innerHTML = "Player 1's turn";
+    if (t_cell.innerHTML === "") {
+      t_cell.innerHTML = "X";
     } else {
-      alert("Pick a free cell");
+      alert("That cell is already chosen");
+      count--;
     }
   }
+  checkWin(table);
+  checkDraw(table);
 }
 
-function winCheck() {
-  var winner = ["X", "O"];
+function checkWin(table) {
+  var xo = ["X", "O"];
 
   for (var i = 0; i < 2; i++) {
-    var r0 = 0;
-    var r1 = 0;
-    var r2 = 0;
-    var r3 = 0;
-    var r4 = 0;
-    var c0 = 0;
-    var c1 = 0;
-    var c2 = 0;
-    var c3 = 0;
-    var c4 = 0;
-    var d1 = 0;
-    var d2 = 0;
+    var pysty1 = 0;
+    var pysty2 = 0;
+    var pysty3 = 0;
+    var pysty4 = 0;
+    var pysty5 = 0;
+    var vaaka1 = 0;
+    var vaaka2 = 0;
+    var vaaka3 = 0;
+    var vaaka4 = 0;
+    var vaaka5 = 0;
+    var viisto1 = 0;
+    var viisto2 = 0;
 
-    for (var j = 0; j < 5; j++) {
-      if (table.rows[0].cells[j].innerHTML === winner[i]) {
-        r0++;
+    for (var t = 0; t < 5; t++) {
+      if (table.rows[0].cells[t].innerHTML === xo[i]) {
+        vaaka1++;
       }
-      if (table.rows[1].cells[j].innerHTML === winner[i]) {
-        r1++;
+      if (table.rows[1].cells[t].innerHTML === xo[i]) {
+        vaaka2++;
       }
-      if (table.rows[2].cells[j].innerHTML === winner[i]) {
-        r2++;
+      if (table.rows[2].cells[t].innerHTML === xo[i]) {
+        vaaka3++;
       }
-      if (table.rows[3].cells[j].innerHTML === winner[i]) {
-        r3++;
+      if (table.rows[3].cells[t].innerHTML === xo[i]) {
+        vaaka4++;
       }
-      if (table.rows[4].cells[j].innerHTML === winner[i]) {
-        r4++;
+      if (table.rows[4].cells[t].innerHTML === xo[i]) {
+        vaaka5++;
       }
-      if (table.rows[j].cells[0].innerHTML === winner[i]) {
-        c0++;
+      if (table.rows[t].cells[0].innerHTML === xo[i]) {
+        pysty1++;
       }
-      if (table.rows[j].cells[1].innerHTML === winner[i]) {
-        c1++;
+      if (table.rows[t].cells[1].innerHTML === xo[i]) {
+        pysty2++;
       }
-      if (table.rows[j].cells[2].innerHTML === winner[i]) {
-        c2++;
+      if (table.rows[t].cells[2].innerHTML === xo[i]) {
+        pysty3++;
       }
-      if (table.rows[j].cells[3].innerHTML === winner[i]) {
-        c3++;
+      if (table.rows[t].cells[3].innerHTML === xo[i]) {
+        pysty4++;
       }
-      if (table.rows[j].cells[4].innerHTML === winner[i]) {
-        c4++;
+      if (table.rows[t].cells[4].innerHTML === xo[i]) {
+        pysty5++;
       }
-      if (table.rows[j].cells[j].innerHTML === winner[i]) {
-        d1++;
+      if (table.rows[t].cells[t].innerHTML === xo[i]) {
+        viisto1++;
       }
-      var reverse = 4 - j;
-      if (table.rows[j].cells[reverse].innerHTML === winner[i]) {
-        d2++;
+      var reverse = 4 - t;
+      if (table.rows[t].cells[reverse].innerHTML === xo[i]) {
+        viisto2++;
       }
-    }
-    if (
-      r0 === 5 ||
-      r1 === 5 ||
-      r2 === 5 ||
-      r3 === 5 ||
-      r4 === 5 ||
-      c0 === 5 ||
-      c1 === 5 ||
-      c2 === 5 ||
-      c3 === 5 ||
-      c4 === 5 ||
-      d1 === 5 ||
-      d2 === 5
-    ) {
-      if (winner[i] === "X") {
-        alert("Player 1 won!");
-        empty();
-      }
-      if (winner[i] === "O") {
-        alert("Player 2 won!");
-        empty();
-      }
-    }
-  }
-}
-
-function drawCheck() {
-  var filled = 0;
-  for (var i = 0; i < table.rows.length; i++) {
-    for (var j = 0; j < table.rows[i].cells.length; j++) {
       if (
-        table.rows[i].cells[j].innerHTML === "X" ||
-        table.rows[i].cells[j].innerHTML === "O"
+        pysty1 === 5 ||
+        pysty2 === 5 ||
+        pysty3 === 5 ||
+        pysty4 === 5 ||
+        pysty5 === 5 ||
+        vaaka1 === 5 ||
+        vaaka2 === 5 ||
+        vaaka3 === 5 ||
+        vaaka4 === 5 ||
+        vaaka5 === 5 ||
+        viisto1 === 5 ||
+        viisto2 === 5
       ) {
-        filled++;
+        if (xo[i] === "X") {
+          alert("Player 1 won!");
+        }
+        if (xo[i] === "O") {
+          alert("Player 2 won!");
+        }
+        clearTable();
       }
     }
   }
-  if (filled === 25) {
-    alert("Draw!");
-    empty();
+}
+
+function checkDraw(table) {
+  var draw_count = 0;
+
+  for (var t = 0; t < table.rows.length; t++) {
+    for (var i = 0; i < table.rows[t].cells.length; i++) {
+      if (
+        table.rows[t].cells[i].innerHTML === "X" ||
+        table.rows[t].cells[i].innerHTML === "O"
+      ) {
+        draw_count++;
+      }
+    }
+  }
+  if (draw_count === 25) {
+    alert("It's a draw!");
+    clearTable();
   }
 }
 
-function empty() {
-  for (var i = 0; i < table.rows.length; i++) {
-    for (var j = 0; j < table.rows[i].cells.length; j++) {
-      table.rows[i].cells[j].innerHTML = "";
+function clearTable() {
+  for (var t = 0; t < table.rows.length; t++) {
+    for (var i = 0; i < table.rows[t].cells.length; i++) {
+      table.rows[t].cells[i].innerHTML = "";
     }
   }
 }
+
+createTable();

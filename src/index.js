3c39dfeee17b8dcf3bf983962bef6536682eb3b7
roosahynninen <63446491+roosahynninen@users.noreply.github.com>
2020-11-08 22:00:46 +0200

    Lollero

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 
 var mongoose = require("mongoose");
-
 var mongoDB =
-  "mongodb+srv://rhynnine:Kissa888!@cluster0.unrpb.mongodb.net/TicTacToe?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+  "mongodb+srv://toimelae:Koodari666@cluster0.5merv.mongodb.net/TicTacToe?retryWrites=true&w=majority";
 
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+// view engine setup
 app.set("views", "./views");
 app.set("view engine", "pug");
 
